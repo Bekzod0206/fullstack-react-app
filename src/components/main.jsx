57a452b71b88bbdd847cb112191ac6214bbf49a2
Bekzod0206 <davronovbekzod20@gmail.com>
2024-10-16 23:27:56 +0500

    added loader

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,11 +1,14 @@
 import { useSelector } from "react-redux"
+import { Loader } from "../ui-components"
 
 function Main() {
 
-  const {articles} = useSelector(state => state.article)
+  const {articles, isLoading} = useSelector(state => state.article)
 
   return (
     <div className="container">
+
+      {isLoading && <Loader />}
 
       <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
         {articles.map(item => (
@@ -16,20 +19,20 @@ function Main() {
                 <rect width="100%" height="100%" fill="#55595c"></rect>
               </svg>
               <div className="card-body">
-                <p className="card-text fw-bold">
+                <p className="card-text fw-bold m-0">
                   {item.title}
                 </p>
                 <p className="card-text">
                   {item.description}
                 </p>
-                <div className="d-flex justify-content-between align-items-center">
-                  <div className="btn-group">
-                    <button type="button" className="btn btn-sm btn-outline-success">View</button>
-                    <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
-                    <button type="button" className="btn btn-sm btn-outline-danger">Delete</button>
-                  </div>
-                  <small className="text-body-secondary fw-bold text-capitalize">{item.author.username}</small>
+              </div>
+              <div className="card-footer d-flex justify-content-between align-items-center">
+                <div className="btn-group">
+                  <button type="button" className="btn btn-sm btn-outline-success">View</button>
+                  <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
+                  <button type="button" className="btn btn-sm btn-outline-danger">Delete</button>
                 </div>
+                <small className="text-body-secondary fw-bold text-capitalize">{item.author.username}</small>
               </div>
             </div>
           </div>
