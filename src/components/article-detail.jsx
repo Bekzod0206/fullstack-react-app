5a38ff99846753f2c5e9330d3e8e8a229ff9e89b
Bekzod0206 <davronovbekzod20@gmail.com>
2024-10-17 23:42:25 +0500

    created article detail ui, viewed data

--- src/components/article-detail.jsx
+++ src/components/article-detail.jsx
@@ -1,13 +1,16 @@
 import { useEffect } from "react"
 import { useParams } from "react-router-dom"
 import ArticleService from "../service/article"
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { getArticleDetailFailure, getArticleDetailStart, getArticleDetailSuccess } from "../slice/article"
+import moment from "moment"
+import { Loader } from "../ui-components"
 
 function ArticleDetail() {
 
   const {slug} = useParams()
-  const dispatch = useDispatch(state => state.article)
+  const dispatch = useDispatch()
+  const { articleDetail, isLoading } = useSelector(state => state.article)
 
   const getArticleDetail = async () => {
     dispatch(getArticleDetailStart())
@@ -16,7 +19,6 @@ function ArticleDetail() {
       dispatch(getArticleDetailSuccess(response.article))
     } catch (error) {
       dispatch(getArticleDetailFailure())
-      console.log(error, 'error')
     }
   }
   
@@ -24,9 +26,46 @@ function ArticleDetail() {
     getArticleDetail()
   }, [slug])
 
-  return (
-    <div>
-      slug: {slug}
+  return isLoading ? <Loader /> : (
+    <div className="jumbotron">
+      <div className="container">
+        <h1 className="display-3">{ articleDetail.title }</h1>
+        <p>
+          {articleDetail.description}
+        </p>
+        <p className="text-muted">
+          <span className="fw-bold">Created at: </span> { moment(articleDetail.createdAt).format('DD MMM, YYYY') }
+        </p>
+        
+        <div className="col-md-6">
+          <div className="card flex-md-row mb-4 box-shadow h-md-250">
+            <div className="card-body d-flex flex-column align-items-start">
+              <strong className="d-inline-block mb-2 text-success">{articleDetail.author.username}</strong>
+              <p className="card-text mb-auto">{articleDetail.author.bio}</p>
+            </div>
+            <svg
+              className="bd-placeholder-img"
+              width="200"
+              height={'100%'}
+              xmlns="htpps://www.w3.org/2000/svg"
+              role="img"
+              aria-label="Placeholder: Thumbnail"
+              preserveAspectRatio="xMidYMid slice"
+              focusable='false'
+            >
+              <title>Placeholder</title>
+              <rect width={'100%'} height={'100%'} fill="#55595c"></rect>
+              <text x={'45%'} y={'53%'} fill="#fff" className="fs-2 text-uppercase p-0 m-0">
+                {articleDetail.author.username[0]}
+              </text>
+            </svg>
+          </div>
+        </div>
+
+        <div>
+          {articleDetail.body}
+        </div>
+      </div>
     </div>
   )
 }
